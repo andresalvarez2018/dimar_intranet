8dc46a6fd056c50f4363d10a655fe13326f5dea7
ELQUIS PACHECO <pachecoelquis@gmail.com>
2024-10-16 07:18:16 -0500

    is create blocks logos

---
+++ web/modules/custom/intranet_core/src/Plugin/Block/LogoDimar.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Drupal\intranet_core\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\file\Entity\File;
+
+/**
+ * Provides a 'logo dimar block' block.
+ *
+ * @Block(
+ *  id = "logo_dimar",
+ *  admin_label = @Translation("Logo Dimar Block"),
+ * )
+ */
+class LogoDimar extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+
+    $form['image'] = [
+      '#type' => 'managed_file',
+      '#title' => t('Imagen'),
+      '#size' => 22,
+      '#upload_location' => 'public://',
+      '#default_value' => $this->configuration['image'] ?? NULL,
+      '#upload_validators' => [
+        'file_validate_extensions' => ['jpg png jpeg webp'],
+      ],
+      '#required' => FALSE,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+    if (isset($values['image'][0])) {
+      $image = File::load($values['image'][0]);
+      $image->setPermanent();
+      $image->save();
+    }
+
+    $this->configuration['image'] = $values['image'];
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['#theme'] = 'logo_dimar_block';
+
+    $image = $this->configuration['image'][0] ?? 0;
+    $url = "";
+    if ($image) {
+      $image = File::load($this->configuration['image'][0]);
+      $url = \Drupal::service('file_url_generator')->generateAbsoluteString($image->getFileUri());
+    }
+
+    $build['#image'] = $url;
+
+    return $build;
+  }
+
+}
